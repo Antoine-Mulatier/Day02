7d5081013a6804adc7a4cec50c62bea099e8b792
francois2sap <francois@desaporta.fr>
2021-06-03 14:18:34 +0200

    ex03 ex04

--- ex03/lemessagedejenny.py
+++ ex03/lemessagedejenny.py
@@ -2,5 +2,4 @@ prenom = input("Bonjour ! Quel est votre prénom ?\n")
 if prenom == "Johnny":
   print ("Hello, my love!")
 else:
-  print ("Salut,", prenom + "!")
-                               
+  print ("Salut,", prenom + "!")  
